39821462b08b131f2e17fd4b4b04625195c555c8
Tsutsumi <tsutsumi@comunidad.unam.mx>
2024-09-03 12:05:54 -0600

    Mensaje de bienvenida

--- main.py
+++ main.py
@@ -6,8 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
-
+    return jsonify([{"Choo Choo": "Welcome to your Flask app 🚅"},{"David": "Bienvenido"},{"Damaris": "Holaa"}, {"Aaron": "Como estas"}, {"Ari": "Wenas"}])
 
 if __name__ == '__main__':
     app.run(debug=True, port=os.getenv("PORT", default=5000))
